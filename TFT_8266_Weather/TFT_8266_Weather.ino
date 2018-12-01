a9b6b2ca675df00bbbfb508490aa8e191acd5a01
MrMinemeet <31434466+MrMinemeet@users.noreply.github.com>
2018-12-01 16:03:07 +0100

    DHT Input hinzugefügt

--- TFT_8266_Weather/TFT_8266_Weather.ino
+++ TFT_8266_Weather/TFT_8266_Weather.ino
@@ -1,39 +1,37 @@
 #include <TFT.h>  // Arduino TFT library
 #include <SPI.h>  // Serial Communication
+#include <DHT.h>  // Sensor Library
 
 // Pins für Arduino Nano
 #define cs   10
 #define dc   9
 #define rst  8
+#define DHT11_PIN 5
 
-// Obejt für Display
+// Object für Display
 TFT TFTscreen = TFT(cs, dc, rst);
+DHT dht(DHT11_PIN, DHT11);
 
 // Char Array für Zahl als Char
 char tempChar[11];
 char humidityChar[11];
 
-float temp = -1.00;
-float humidity = -1.00;
+float temp;
+float humidity;
 
 void setup() {
   // TFT Display initialisieren
   TFTscreen.begin();
-
-  // Hintergrund auf Schwarz setzen
-  TFTscreen.background(0, 0, 0);
-  // Textfarbe auf Weiß setzen
-  TFTscreen.stroke(255, 255, 255);
-  
-  TFTscreen.setTextSize(2);
-  // Text oben links ausegben
-  TFTscreen.text("Temp.:\n", 0, 0);
-  TFTscreen.text("Temp.:\n", 0, 50);
-  
   TFTscreen.setTextSize(2);
 }
 
 void loop() {
+
+  TFTscreen.background(0, 0, 0);
+
+  
+  temp = dht.readTemperature();
+  humidity = dht.readHumidity();
   
 
   TFTscreen.stroke(255, 255, 255);
@@ -41,22 +39,23 @@ void loop() {
   itoa(temp, tempChar, 10);
   
   itoa(humidity, humidityChar, 10);
+
+  
+  TFTscreen.text("Temp.:\n", 0, 0);
   
   // Textfarbe auf Weiß setzen und Temp ausgeben
-  TFTscreen.text(tempChar, 0, 25);
+  TFTscreen.text(tempChar, 0, 32);
+
+  
+  TFTscreen.text("Temp.:\n", 0,64);
 
   
   // Long in Char Array umwandeln
   itoa(humidity, tempChar, 10);
   
   // Textfarbe auf Weiß setzen und Temp ausgeben
-  TFTscreen.text(humidityChar, 0, 70);
+  TFTscreen.text(humidityChar, 0, 96);
 
-  delay(10000);
-
-  // Löschen
-  TFTscreen.stroke(0, 0, 0);
-  TFTscreen.text(tempChar, 0, 25);
-  TFTscreen.text(humidityChar, 0, 70);
+  delay(5000);
 
 }
